f31814117b6d9490c148ad790b85219c7685c9de
ibrahimhanifceker <83652655+ibrahimhanifceker@users.noreply.github.com>
2024-06-24 15:05:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -12,29 +12,66 @@ serv.listen(process.env.PORT || 1453);
 console.log('Server Started');
 
 var Sockets = {};
+var Players = {};
+
+var Player = function(id){
+    var self = {
+        x:250,
+        y:250,
+        id:id,
+        speed:10,
+        right:false,
+        down:false,
+        left:false,
+        up:false
+    }
+    self.updatePosition = function(){
+        self.x += self.speed * self.right;
+        self.x -= self.speed * self.left;
+        self.y += self.speed * self.down;
+        self.y -= self.speed * self.up;
+    }
+    return self;
+}
 
 var io = require('socket.io')(serv,{});
 io.sockets.on('connection', function(socket){
     socket.id = Math.random();
     console.log('socket connection' + socket.id);
-    socket.x = 250;
-    socket.y = 250;
     Sockets[socket.id] = socket;
+
+    var player = Player(socket.id);
+    Players[socket.id] = player;
+
+    socket.on('key_press', function(data){
+        if(data.key == 'R'){
+            player.right = data.is_pressed;
+        }
+        else if(data.key=='L'){
+            player.left = data.is_pressed;
+        }
+        else if(data.key=='D'){
+            player.down = data.is_pressed;
+        }
+        else if(data.key=='U'){
+            player.up = data.is_pressed;
+        }
+    })
 
     socket.on('disconnect', function(){
         delete Sockets[socket.id];
+        delete Players[socket.id];
     })
 });
 
 setInterval(function(){
     var pack = [];
-    for(var i in Sockets){
-        var socket = Sockets[i];
-        socket.x++;
-        socket.y++;
+    for(var i in Players){
+        var player = Players[i];
+        player.updatePosition();
         pack.push({
-            x:socket.x,
-            y:socket.y
+            x:player.x,
+            y:player.y
         });
     }
     for(var i in Sockets){
